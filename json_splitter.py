10a2e1b2b01fae929556963a3315e4a3ebbab256
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2022-02-02 15:35:56 -0300

    Revert "utilização do modulo rich para print colorido"
    This reverts commit ef9f3a5f2556711b7ff2ea353fe2d0482f6b0eff.

--- json_splitter.py
+++ json_splitter.py
@@ -1,14 +1,13 @@
 import json
 import numpy as np
 import sys
-import rich
 
 if len(sys.argv) < 4:
     raise ValueError("Número de argumentos menor que 4")
     exit()
 
 try:
-    with open(sys.argv[1], "r", encoding="utf-8") as file:
+    with open(sys.argv[1], "r", encoding='utf-8') as file:
         file = json.load(file)
 except FileNotFoundError:
     raise FileNotFoundError(f"Arquivo JSON --> {sys.argv[1]} base inválido!")
@@ -20,13 +19,10 @@ num_parts = len(nomes)
 split_list = np.array_split(file, num_parts)
 
 for index in range(num_parts):
-    with open(f"{nomes[index]}.json", "w", encoding="utf-8") as file_out:
-        json.dump(
-            split_list[index].tolist(), file_out, indent=2, ensure_ascii=False
-        )
-
-rich.print(
-    f"\n:tada: Arquivo '{sys.argv[1]}' dividido em {num_parts} partes:\n"
-)
+    with open(f"{nomes[index]}.json", "w", encoding='utf-8') as file_out:
+        json.dump(split_list[index].tolist(), file_out, indent = 2, ensure_ascii=False)
+  
+print(f"Arquivo {sys.argv[1]} dividido em {num_parts} partes")
 for nome in nomes:
-    rich.print(f">>> [bold]./{nome}.json[/bold]")
+    print(f"{nome}.json")
+            
